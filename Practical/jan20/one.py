fcabe53bbdabaef548317f3f0265cce3b2cee481
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-01-20 14:03:35 +0530

    jan20 q1 done

---
+++ Practical/jan20/one.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df = pd.read_csv("Practical\\jan20\\el.csv")
+
+def percentf():
+    docno = df.groupby("Profession").sum().loc["Doctor"]["Sno."]
+    engno = df.groupby("Profession").sum().loc["Engineer"]["Sno."]
+    tot = df.groupby("Profession").sum()["Sno."].sum()
+
+    print(f"Percentage of Engineers: {round(((engno/tot)*100), 2)}%")
+    print(f"Percentage of Doctors: {round(((docno/tot)*100), 2)}%")
+
+
+def compare_add():
+    tot = df["Salary"].sum()
+    df["Salary Percentage"] = round(((df["Salary"]/tot)*100), 2)
+    print(df)
+
+compare_add()
+
